139e1a8f2f42d1a0ee9ea1d622567b19acd2325b
Bjarnos <137303555+Bjarnos@users.noreply.github.com>
2024-12-06 12:04:45 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+print("alive")
+from flask import Flask
+from flask_cors import CORS
+import os
+import requests
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://bjarnos.github.io"}})
+
+@app.route('/')
+def hello_world():
+    return "hello"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
